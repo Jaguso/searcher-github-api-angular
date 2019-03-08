53b5358f6f8b2903af917a2b892d30eeba825257
Jaguso <jagutzo@gmail.com>
2019-03-07 18:47:36 -0600

    with this you can fetch users from github API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { UsersService } from './users.service';
 
@@ -24,7 +24,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  uri = 'https://api.github.com'
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get(`${this.uri}/users`);
+  }
 }
