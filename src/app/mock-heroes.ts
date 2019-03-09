4a30289e3ce4949c40ff18dd204226c78dec1734
Jaguso <jagutzo@gmail.com>
2019-03-08 20:05:10 -0600

    escentially what you need to make the for list

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
+    { id: 11, name: 'Mr. Bean' },
     { id: 12, name: 'Narco' },
     { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
+    { id: 14, name: 'Clitoris' },
+    { id: 15, name: 'Magneto' },
+    { id: 16, name: 'Rubens' },
+    { id: 17, name: 'Dynamite' },
+    { id: 18, name: 'Dr IQnes' },
+    { id: 19, name: 'Magmanaman' },
     { id: 20, name: 'Tornado' }
 ];
